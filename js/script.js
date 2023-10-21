b3a73d346a98d2e899a0205c5479c832a34e2813
markbosire <markbosirekenyariri@gmail.com>
2023-10-22 00:02:23 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,185 @@
+// Sample data
+const queencakes = [
+    { name: "Soft Queencake", price: "10 KSH", image: "img/Queencakes/queen1.jpg" },
+    { name: "Muffin", price: "20 KSH", image: "img/Queencakes/queen2.jpg" },
+    { name: "Maffin l2", price: "50S KSH", image: "img/Queencakes/queen3.jpg" },
+    { name: "Gluten-Free Mini Cheesecakes", price: "50 KSH", image: "img/Queencakes/queen4.jpg" },
+    { name: "Maffin l3", price: "50 KSH", image: "img/Queencakes/queen5.jpg" },
+    { name: "Queen Cake 6", price: "50 KSH", image: "img/Queencakes/queen6.jpg" },
+    { name: "Queen Cake 7", price: "50 KSH", image: "img/Queencakes/queen7.jpg" },
+    { name: "Queen Cake 8", price: "50 KSH", image: "img/Queencakes/queen8.jpg" },
+    { name: "Queen Cake 9", price: "50 KSH", image: "img/Queencakes/queen9.jpg" },
+    { name: "Queen Cake 10", price: "50 KSH", image: "img/Queencakes/queen10.jpg" },
+    { name: "Queen Cake 11", price: "50 KSH", image: "img/Queencakes/queen11.jpg" },
+    { name: "Queen Cake 12", price: "50 KSH", image: "img/Queencakes/queen12.jpg" },
+    { name: "Queen Cake 13", price: "50 KSH", image: "img/Queencakes/queen13.jpg" },
+    { name: "Queen Cake 14", price: "50 KSH", image: "img/Queencakes/queen14.jpg" },
+    { name: "Queen Cake 15", price: "50 KSH", image: "img/Queencakes/queen15.jpg" },
+    { name: "Queen Cake 16", price: "50 KSH", image: "img/Queencakes/queen16.jpg" },
+    { name: "Queen Cake 17", price: "50 KSH", image: "img/Queencakes/queen17.jpg" },
+    { name: "Queen Cake 18", price: "50 KSH", image: "img/Queencakes/queen18.jpg" },
+    { name: "Queen Cake 19", price: "50 KSH", image: "img/Queencakes/queen19.jpg" },
+    { name: "Queen Cake 20", price: "50 KSH", image: "img/Queencakes/queen20.jpg" },
+    { name: "Queen Cake 21", price: "50 KSH", image: "img/Queencakes/queen21.jpg" },
+    { name: "Queen Cake 22", price: "50 KSH", image: "img/Queencakes/queen22.jpg" },
+    { name: "Queen Cake 23", price: "50 KSH", image: "img/Queencakes/queen23.jpg" },
+    { name: "Queen Cake 24", price: "50 KSH", image: "img/Queencakes/queen24.jpg" },
+    { name: "Queen Cake 25", price: "50 KSH", image: "img/Queencakes/queen25.jpg" },
+    { name: "Queen Cake 26", price: "50 KSH", image: "img/Queencakes/queen26.jpg" },
+    { name: "Queen Cake 27", price: "50 KSH", image: "img/Queencakes/queen27.jpg" },
+    { name: "Queen Cake 28", price: "50 KSH", image: "img/Queencakes/queen28.jpg" }
+    // Add more Queen Cakes as needed
+];
+const birthdaycakes = [
+    { name: "Birthday Cake with White Vanilla Frosting", price: "1500 KSH", image: "img/Birthday/cake1.jpg" },
+    { name: "Pineapple White Forest", price: "1200 KSH", image: "img/Birthday/cake2.jpg" },
+    { name: "Black Forest1", price: "1800 KSH", image: "img/Birthday/cake3.jpg" },
+    { name: "Black Forest2", price: "1300 KSH", image: "img/Birthday/cake4.jpg" },
+    { name: "Vanilla Cake", price: "1600 KSH", image: "img/Birthday/cake5.jpg" },
+    { name: "Choclate Cake1", price: "1100 KSH", image: "img/Birthday/cake6.jpg" },
+    { name: "Choclate Cake2", price: "1400 KSH", image: "img/Birthday/cake7.jpg" },
+    { name: "Black Forest3", price: "1700 KSH", image: "img/Birthday/cake8.jpg" },
+    { name: "Red Velvate Cake1", price: "1900 KSH", image: "img/Birthday/cake9.jpg" },
+    { name: "Red Velvate2", price: "2000 KSH", image: "img/Birthday/cake10.jpg" },
+    { name: "Red Velvate3", price: "1250 KSH", image: "img/Birthday/cake11.jpg" }
+    // Add more birthday cakes as needed
+];
+
+const breads = [
+    { name: "Soft Bread", price: "55 KSH", image: "img/Bread/Bread1.jpg" },
+    { name: "White Bread", price: "50 KSH", image: "img/Bread/Bread2.jpg" },
+    { name: "French Bread", price: "60 KSH", image: "img/Bread/Bread3.jpg" },
+    { name: "Non sliced Bread", price: "50 KSH", image: "img/Bread/Bread4.jpg" },
+    { name: "brioche Bread", price: "80 KSH", image: "img/Bread/Bread5.jpg" },
+    { name: "Challah Bread", price: "80 KSH", image: "img/Bread/Bread6.jpg" },
+    { name: " Low calorie whole wheat bread", price: "50 KSH", image: "img/Bread/Bread7.jpg" },
+    { name: "Milk Bread", price: "55 KSH", image: "img/Bread/Bread8.jpg" }
+    // Add more bread products as needed
+];
+const cookies = [
+    { name: "Vanilla Cookies", price: "10 KSH", image: "img/Cookies/cookie1.jpg" },
+    { name: "Coconut Cookies", price: "10 KSH", image: "img/Cookies/cookie2.jpg" },
+    { name: "Ginger Cookies", price: "15 KSH", image: "img/Cookies/cookie3.jpg" },
+    { name: "Batter Cookies", price: "20 KSH", image: "img/Cookies/cookie4.jpg" },
+    { name: "Cookie 5", price: "15 KSH", image: "img/Cookies/cookie5.jpg" },
+    { name: "Cookie 6", price: "10 KSH", image: "img/Cookies/cookie6.jpg" },
+    { name: "Cookie 7", price: "10 KSH", image: "img/Cookies/cookie7.jpg" },
+    { name: "Cookie 8", price: "10 KSH", image: "img/Cookies/cookie8.jpg" }
+    // Add more cookie products as needed
+];
+const pizzas = [
+    { name: "Pizza 1", price: "250 KSH", image: "img/Pizza/pizza1.jpg" },
+    { name: "Pizza 2", price: "280 KSH", image: "img/Pizza/pizza2.jpg" },
+    { name: "Pizza 3", price: "300 KSH", image: "img/Pizza/pizza3.jpg" },
+    { name: "Pizza 4", price: "270 KSH", image: "img/Pizza/pizza4.jpg" }
+    // Add more pizza products as needed
+];
+const weddingcakes = [
+    { name: "Wedding Cake 1", price: "3000 KSH", image: "img/Wedding/cake1.jpg" },
+    { name: "Wedding Cake 2", price: "3500 KSH", image: "img/Wedding/cake2.jpg" },
+    { name: "Wedding Cake 3", price: "4000 KSH", image: "img/Wedding/cake3.jpg" },
+    { name: "Wedding Cake 4", price: "3200 KSH", image: "img/Wedding/cake4.jpg" },
+    { name: "Wedding Cake 5", price: "3800 KSH", image: "img/Wedding/cake5.jpg" },
+    { name: "Wedding Cake 6", price: "3100 KSH", image: "img/Wedding/cake6.jpg" },
+    { name: "Wedding Cake 7", price: "3600 KSH", image: "img/Wedding/cake7.jpg" },
+    { name: "Wedding Cake 8", price: "3400 KSH", image: "img/Wedding/cake8.jpg" }
+    // Add more wedding cakes as needed
+];
+
+
+
+
+// Function to render the product grid
+function renderProductGrid(id,array) {
+    const productGrid = document.getElementById(id);
+
+    array.forEach((product) => {
+        const gridItem = document.createElement("div");
+        gridItem.classList.add("grid-item");
+
+        const productImage = document.createElement("img");
+        productImage.src = product.image;
+        productImage.alt = product.name;
+
+        const productName = document.createElement("p");
+        productName.textContent = product.name;
+
+        const productPrice = document.createElement("p");
+        productPrice.textContent = product.price;
+
+        const quantityInput = document.createElement("input");
+        quantityInput.type = "number";
+        quantityInput.min = 1;
+        quantityInput.value = 1;
+
+        const whatsappButton = document.createElement("button");
+        whatsappButton.classList.add("button");
+        whatsappButton.textContent = "Order on WhatsApp";
+
+        whatsappButton.addEventListener("click", () => {
+            const quantity = quantityInput.value;
+            const message = `I want ${quantity} ${product.name} please and the price will add up to ${quantity*parseInt(product.price)}`;
+            const phoneNumber = "+254712244969"; // Replace with your WhatsApp number
+            const whatsappLink = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(message)}`;
+
+            window.location.href = whatsappLink;
+        });
+
+        gridItem.appendChild(productImage);
+        gridItem.appendChild(productName);
+        gridItem.appendChild(productPrice);
+        gridItem.appendChild(quantityInput);
+        gridItem.appendChild(whatsappButton);
+
+        productGrid.appendChild(gridItem);
+    });
+}
+
+// Call the function to render the product grid
+renderProductGrid("product-grid-cake",queencakes);
+renderProductGrid("product-grid-birthday",birthdaycakes);
+renderProductGrid("product-grid-bread",breads);
+renderProductGrid("product-grid-cookies",cookies);
+renderProductGrid("product-grid-wedding",weddingcakes);
+renderProductGrid("product-grid-pizza",pizzas);
+function sendEmail(event) {
+    event.preventDefault();
+    let name = document.getElementById("name").value;
+    let email = "karimthedream254@gmail.com"
+    let email2 = document.getElementById("email").value;
+    let subject = document.getElementById("subject").value;
+    let message = document.getElementById("message").value;
+    var toast = document.querySelector('.toast');
+
+    // Get the toast body element
+    var toastBody = toast.querySelector('.toast-body');
+
+    // Set the message in the toast body
+   
+    console.log("dasd")
+  
+    Email.send({
+      Host : "smtp.elasticemail.com",
+      Username : "karimthedream254@gmail.com",
+      Password : "B860726569C04551D7562F3DECB4D5505E10",
+      From: email,
+      To: "follextech@gmail.com",
+    
+      Subject: subject,
+      Body: "Name: " + name + "<br>Email: " + email2 + "<br>Message: " + message,
+    }).then(
+      function (response) {
+        if (response === "OK") {
+          document.querySelector(".message").style.display = "block";
+          document.querySelector(".sent-message").style.display = "block";
+        } else {
+          throw new Error("Error: " + response);
+        }
+      },
+      function (error) {
+        document.querySelector(".error-message").style.display = "block";
+        console.log(error);
+      }
+    );
+  }
+
