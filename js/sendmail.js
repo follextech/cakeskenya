9bc725f11248f7f5f06d4b5502fb70943c140294
follextech <140881619+follextech@users.noreply.github.com>
2024-06-15 21:35:16 +0300

    Update sendmail.js

--- js/sendmail.js
+++ js/sendmail.js
@@ -3,6 +3,7 @@ document.getElementById('contactForm').addEventListener('submit', function(e) {
 
   var form = e.target;
   var formData = new FormData(form);
+  console.log(formData)
 
   fetch('https://formsubmit.co/6c6a01e95c660b0223a57c443b5e9e92', {
     method: 'POST',
